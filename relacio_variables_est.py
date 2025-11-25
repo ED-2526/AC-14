782a7139cb70efa2f17ef2f6b8a70eddc9ddd3d3
Pol <1706768@uab.cat>
2025-11-25 12:46:03 +0100

    Relacio variables estudiants

--- relacio_variables_est.py
+++ relacio_variables_est.py
@@ -3,14 +3,12 @@ import pandas as pd
 
 
 df_estudiants = pd.read_csv('estudiants_med.csv')
-
-
 df_cat = df_estudiants[['sex', 'psyt', 'health', 'year']]
-
 
 chi2_table = pd.DataFrame(columns=['Variable 1', 'Variable 2', 'Chi-Square', 'p-value'])
 
-
+#Fem test khi-quadrat per sex amb psyt, year amb health i sex amb health
+#Analitzem el valor de p-value, si es menor a 0.05 hi ha relació significativa
 for col1, col2 in [('sex', 'psyt'), ('year', 'health'), ('sex', 'health')]:
     chi2, p, dof, ex = chi2_contingency(pd.crosstab(df_estudiants[col1], df_estudiants[col2]))
     
@@ -21,7 +19,7 @@ for col1, col2 in [('sex', 'psyt'), ('year', 'health'), ('sex', 'health')]:
 print("Taula de Chi-Square per a les variables dels estudiants:")
 print(chi2_table)
 
-
+#Test pearson per dades numeriques
 correlation_stud_h_health = df_estudiants['stud_h'].corr(df_estudiants['health'])
 print(f"Correlació entre hores d'estudi i salut: {correlation_stud_h_health}")
 
